04ef6f726aae4bd90db5d55bfeb6755f7420b283
Ian Melendez <imelendez@preludedynamics.com>
2023-03-01 00:46:48 -0600

    inc lit

--- src/Trees.hs
+++ src/Trees.hs
@@ -6,6 +6,7 @@
 module Trees where 
 
 import GHC.Types (Constraint)
+import Data.Void
 
 data Exp = Lit Integer 
          | Var Var
@@ -31,3 +32,27 @@ data XExp x = XLit (XXLit x) Integer
             | XAbs (XXAbs x) Var Exp
             | XApp (XXApp x) Exp Exp
             | XExp (XXExp x)
+
+-- UD - undecorated
+
+type instance XXLit UD = Void
+type instance XXVar UD = Void
+type instance XXAnn UD = Void
+type instance XXAbs UD = Void
+type instance XXApp UD = Void
+type instance XXExp UD = Void
+
+data UD
+
+type XExpUD = XExp UD
+
+void :: Void
+void = error "void"
+
+incLit :: Exp -> Exp
+incLit (Lit i) = Lit (i + 1)
+incLit e = e
+
+incLitX :: XExpUD -> XExpUD
+incLitX (XLit _ i) = XLit void (i + 1)
+incLitX e = e
